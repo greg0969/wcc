74c640752395a53a99933141eaec60f11189352f
Greg <gregthue@gmail.com>
2021-09-15 13:53:24 +0200

    path

--- js/index.js
+++ js/index.js
@@ -36,7 +36,7 @@ const displayCampingcar = (ArrayOfCampingcar) => {
 
 
 const getData = async () =>
-    await fetch("../data.json")
+    await fetch("./data.json")
       .then((res) => res.json())
       .catch((error) => console.log("erreur"))
       
