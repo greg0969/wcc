9b08154095ff902105c28de26ae64243b72e013f
Greg <gregthue@gmail.com>
2021-09-15 18:01:29 +0200

    modif

--- js/ficheTech.js
+++ js/ficheTech.js
@@ -15,7 +15,14 @@ createhtml() {
             <p class="ficheTechnique__critères">Année-modèle : ${this.criteres[0]}, 
             Mise en circulation : ${this.criteres[1]}, Kilométrage : ${this.criteres[2]}, Référence : ${this.criteres[3]}
             </p>
-            <p class="ficheTechnique__description">${this.description}</p>
+            <div>
+            <p class="ficheTechnique__description">${this.description[0]}</p>
+            <p class="ficheTechnique__description">${this.description[1]}</p>
+            <p class="ficheTechnique__description">${this.description[2]}</p>
+            <p class="ficheTechnique__description">${this.description[3]}</p>
+            <p class="ficheTechnique__description">${this.description[4]}</p>
+            <p class="ficheTechnique__description">${this.description[5]}</p>
+            </div>     
         </div>
         <button class="acheter" role="acheter">Je suis intéressé</button>
 	`
